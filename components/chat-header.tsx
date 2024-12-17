3f64f4dfd4c52fa38aa522f9f60c01362d396c75
anikobalogh <aniko.balogh@emeal.nttdata.com>
2024-12-17 13:16:00 +0200

    animation for logo - has to be tested

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -48,21 +48,23 @@ export function ChatHeader({ userName }: { userName: string }) {
   return (
     <div className="w-full flex justify-between items-center">
       <Link className="flex items-center justify-center" href="/">
-        <img
-          src="/images/logomark.svg"
-          alt="Image 1"
-          width="48px"
-          className="block"
-        />
-        <motion.img
-          src="/images/logotype.svg"
-          width="120px"
-          alt="Image 2"
-          className="relative top-0 left-0"
-          initial={{ opacity: 0, x: -50 }}
-          whileHover={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.5, ease: "linear" }}
-        />
+        <div className="relative group w-[206px] h-[40.90px] overflow-hidden">
+          <div
+              className="elem1 w-full h-full flex items-center justify-start text-white font-bold z-100"
+          >
+            <img
+                src="/images/logomark.svg"
+                alt="Image 1"
+                width="48px"
+                className="block"
+            />
+          </div>
+          <img
+              src="/images/logotype.svg"
+              alt="Sliding Image"
+              className="absolute top-[13px] left-0 w-[120px] h-[14px] opacity-0 -translate-x-full group-hover:translate-x-[60px] group-hover:opacity-100 transition-all duration-500 ease-in-out z-0"
+          />
+        </div>
       </Link>
 
       <div
