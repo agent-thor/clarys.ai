947defdb5f541d366f7573e9fcaaca61ff5893c6
anikobalogh <aniko.balogh@emeal.nttdata.com>
2024-12-18 13:19:30 +0200

    menu fixed

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -1,48 +1,9 @@
 "use client";
 
-import {useSidebar} from "./ui/sidebar";
-import {signOut} from "next-auth/react";
-import {useRef, useState} from "react";
 import Link from "next/link";
-import {saveTourCompleted, saveTourNeeded} from "@/app/(chat)/actions";
-import {useRouter} from "next/navigation";
+import Menu from "@/components/menu";
 
 export function ChatHeader({ userName }: { userName: string }) {
-  const router = useRouter();
-  const [activeMenu, setActiveMenu] = useState<"menu1" | "menu2" | null>(null);
-  const dropdownRef = useRef<HTMLDivElement | null>(null);
-
-  const handleMenuClick = (menu: "menu1" | "menu2") => {
-    setActiveMenu((prev) => (prev === menu ? null : menu));
-  };
-
-  const handleLogout = () => {
-    signOut({
-      redirectTo: "/",
-    });
-  };
-
-  const handleWhyClarys = () => {
-    saveTourNeeded(true);
-    saveTourCompleted(false);
-    setTimeout(() => {
-      router.push("/");
-    }, 500);
-  };
-
-  const handleBlur = (e: React.FocusEvent) => {
-    setTimeout(() => {
-      if (
-        dropdownRef.current &&
-        !dropdownRef.current.contains(e.relatedTarget as Node)
-      ) {
-        setActiveMenu(null);
-      }
-    }, 100);
-  };
-
-  const { toggleSidebar } = useSidebar();
-
   return (
     <div className="w-full flex justify-between items-center">
       <Link className="flex items-center justify-center" href="/">
@@ -62,69 +23,7 @@ export function ChatHeader({ userName }: { userName: string }) {
           />
         </div>
       </Link>
-
-      <div
-        className="relative buttonShadow px-8 py-2 rounded-2xl flex flex-row gap-4"
-        onMouseDown={(e) => e.preventDefault()}
-        onBlur={handleBlur}
-      >
-        <button
-          className="flex items-center justify-center space-x-2 focus:outline-none py-2 px-8 gap-2 hover:underline"
-          onClick={() => handleMenuClick("menu1")}
-        >
-          <span className="hidden sm:block text-[12px] leading-4">
-            {userName}
-          </span>
-          <img src="/images/user.svg" alt="User" className="w-4 h-4" />
-        </button>
-        <button
-          className="flex items-center justify-center space-x-2 focus:outline-none py-2 px-4 gap-2 w-[120px]  hover:underline"
-          onClick={() => handleMenuClick("menu2")}
-        >
-          <span className="hidden sm:block text-[12px] leading-4">Menu</span>
-          <img src="/images/menu.svg" alt="Menu" className="w-4 h-4" />
-        </button>
-
-        {activeMenu === "menu1" && (
-          <div
-            ref={dropdownRef}
-            className="absolute top-12 right-[168px] mt-2 bg-white text-[12px] leading-4 buttonShadow p-4 rounded-2xl"
-          >
-            <ul className="py-1">
-              <li
-                className="px-8 py-4 hover:underline cursor-pointer"
-                onClick={handleLogout}
-              >
-                Logout
-              </li>
-              <li className="px-8 py-4 text-disabled">Connect Wallet</li>
-              <li
-                className="px-8 py-4 hover:underline cursor-pointer"
-                onClick={toggleSidebar}
-              >
-                See Last Queries
-              </li>
-            </ul>
-          </div>
-        )}
-
-        {activeMenu === "menu2" && (
-          <div
-            ref={dropdownRef}
-            className="absolute top-12 right-0 mt-2 bg-white text-[12px] leading-4 buttonShadow p-4 rounded-2xl"
-          >
-            <ul className="py-1">
-              <li
-                className="px-8 py-4 hover:underline cursor-pointer"
-                onClick={handleWhyClarys}
-              >
-                Why Clarys.AI
-              </li>
-              <li className="px-8 py-4 text-disabled">All Data</li>
-            </ul>
-          </div>
-        )}
-      </div>
+      <Menu userName={userName}></Menu>
     </div>
   );
 }
---
+++ components/menu.tsx
@@ -0,0 +1,128 @@
+import { useState, useEffect, useRef } from "react";
+import { signOut } from "next-auth/react";
+import { useSidebar } from "@/components/ui/sidebar";
+import { saveTourCompleted, saveTourNeeded } from "@/app/(chat)/actions";
+import { router } from "next/client";
+
+const Menu = ({ userName }: { userName: string }) => {
+  const [activeMenu, setActiveMenu] = useState<string | null>(null);
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+
+  const menu1Ref = useRef<HTMLDivElement | null>(null);
+  const menu2Ref = useRef<HTMLDivElement | null>(null);
+  const submenuRef = useRef<HTMLDivElement | null>(null);
+
+  const handleMenuClick = (menu: string) => {
+    if (activeMenu === menu) {
+      setIsSubMenuOpen(!isSubMenuOpen);
+    } else {
+      setActiveMenu(menu);
+      setIsSubMenuOpen(true);
+    }
+  };
+
+  const handleClickOutside = (e: MouseEvent) => {
+    if (
+      menu1Ref.current &&
+      !menu1Ref.current.contains(e.target as Node) &&
+      menu2Ref.current &&
+      !menu2Ref.current.contains(e.target as Node) &&
+      submenuRef.current &&
+      !submenuRef.current.contains(e.target as Node)
+    ) {
+      setActiveMenu(null);
+      setIsSubMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const handleLogout = () => {
+    signOut({
+      redirectTo: "/",
+    });
+  };
+  const { toggleSidebar } = useSidebar();
+
+  const handleWhyClarys = () => {
+    saveTourNeeded(true);
+    saveTourCompleted(false);
+    setTimeout(() => {
+      router.push("/");
+    }, 500);
+  };
+
+  return (
+    <>
+      <div className="relative buttonShadow px-8 py-2 rounded-2xl flex flex-row gap-4">
+        <div
+          className="relative"
+          ref={menu1Ref}
+          onClick={() => handleMenuClick("menu1")}
+        >
+          <button className="flex items-center justify-center space-x-2 focus:outline-none py-2 px-8 gap-2 hover:underline">
+            <span className="hidden sm:block text-[12px] leading-4">
+              {userName}
+            </span>
+            <img src="/images/user.svg" alt="User" className="w-4 h-4" />
+          </button>
+          {activeMenu === "menu1" && isSubMenuOpen && (
+            <div
+              ref={submenuRef}
+              className="absolute top-10 right-0 mt-2 bg-white text-[12px] leading-4 buttonShadow p-4 rounded-2xl min-w-[200px]"
+            >
+              <ul className="py-1">
+                <li
+                  className="px-8 py-4 hover:underline cursor-pointer"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </li>
+                <li className="px-8 py-4 text-disabled">Connect Wallet</li>
+                <li
+                  className="px-8 py-4 hover:underline cursor-pointer"
+                  onClick={toggleSidebar}
+                >
+                  See Last Queries
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+        <div
+          className="relative"
+          ref={menu2Ref}
+          onClick={() => handleMenuClick("menu2")}
+        >
+          <button className="flex items-center justify-center space-x-2 focus:outline-none py-2 px-4 gap-2 w-[120px]  hover:underline">
+            <span className="hidden sm:block text-[12px] leading-4">Menu</span>
+            <img src="/images/menu.svg" alt="Menu" className="w-4 h-4" />
+          </button>
+          {activeMenu === "menu2" && isSubMenuOpen && (
+            <div
+              ref={submenuRef}
+              className="absolute top-10 right-[-32px] mt-2 bg-white text-[12px] leading-4 buttonShadow p-4 rounded-2xl min-w-[180px]"
+            >
+              <ul className="py-1">
+                <li
+                  className="px-8 py-4 hover:underline cursor-pointer"
+                  onClick={handleWhyClarys}
+                >
+                  Why Clarys.AI
+                </li>
+                <li className="px-8 py-4 text-disabled">All Data</li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Menu;
