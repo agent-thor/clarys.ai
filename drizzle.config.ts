8996b88a52efb8acd59d4b7397cfe3bb6bdc2d5e
TibeCiucNtt <tibeciuc@emeal.nttdata.com>
2025-03-13 09:24:08 +0200

    update tsconfig

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,29 @@
 import { config } from 'dotenv';
-import { defineConfig } from 'drizzle-kit';
+import { Config } from 'drizzle-kit';
 
 config({
   path: '.env.local',
 });
 
-export default defineConfig({
+export default {
   schema: './lib/db/schema.ts',
   out: './lib/db/migrations',
-  dialect: 'postgresql',
+  // dialect: 'postgresql',
   dbCredentials: {
-    // biome-ignore lint: Forbidden non-null assertion.
-    url: process.env.POSTGRES_URL!,
+    host: process.env.POSTGRES_URL!,
+    port: parseInt(process.env.POSTGRES_URL_PORT!),
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE!
   },
-});
+} satisfies Config;
+
+// export default defineConfig({
+//   schema: './lib/db/schema.ts',
+//   out: './lib/db/migrations',
+//   dialect: 'postgresql',
+//   dbCredentials: {
+//     // biome-ignore lint: Forbidden non-null assertion.
+//     url: process.env.POSTGRES_URL!,
+//   },
+// });
